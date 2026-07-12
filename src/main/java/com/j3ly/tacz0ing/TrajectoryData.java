042bf225b2f43c7c4296eb9e36cf994a203b7204
i is evil duck <armsit1479@gmail.com>
2026-07-11 22:24:46 -0700

    persist trajectory offset in player NBT to survive reload

--- src/main/java/com/j3ly/tacz0ing/TrajectoryData.java
+++ src/main/java/com/j3ly/tacz0ing/TrajectoryData.java
@@ -1,26 +1,28 @@
 package com.j3ly.tacz0ing;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TrajectoryData {
-    private static final Map<java.util.UUID, Float> playerOffsets = new HashMap<>();
+    private static final String NBT_KEY = "tacz0ing_trajectory_offset";
 
     public static final float MAX_OFFSET = 5.0f;
     public static final float MIN_OFFSET = -5.0f;
     public static final float STEP = 0.01f;
 
     public static float getOffset(Player player) {
-        return playerOffsets.getOrDefault(player.getUUID(), 0.0f);
+        CompoundTag data = player.getPersistentData();
+        if (data.contains(NBT_KEY)) {
+            return data.getFloat(NBT_KEY);
+        }
+        return 0.0f;
     }
 
     public static void setOffset(Player player, float offset) {
         offset = Math.max(MIN_OFFSET, Math.min(MAX_OFFSET, offset));
-        playerOffsets.put(player.getUUID(), offset);
+        player.getPersistentData().putFloat(NBT_KEY, offset);
     }
 
     public static void adjustOffset(Player player, float delta) {
@@ -29,7 +31,15 @@ public class TrajectoryData {
     }
 
     public static void clearOffset(Player player) {
-        playerOffsets.remove(player.getUUID());
+        player.getPersistentData().remove(NBT_KEY);
+    }
+
+    @SubscribeEvent
+    public void onPlayerClone(PlayerEvent.Clone event) {
+        float offset = event.getOriginal().getPersistentData().getFloat(NBT_KEY);
+        if (offset != 0.0f) {
+            event.getEntity().getPersistentData().putFloat(NBT_KEY, offset);
+        }
     }
 
     @SubscribeEvent
